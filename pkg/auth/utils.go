86563aee02725dd1cd2abeda00525841ae8788da
Eds Tardo <edsarino.tardo@gmail.com>
2025-03-08 21:54:47 +0800

    auth: added util func for checking hashed password

--- pkg/auth/utils.go
+++ pkg/auth/utils.go
@@ -8,3 +8,8 @@ func hashPassword(password string) (string, error) {
 	b, err := bcrypt.GenerateFromPassword([]byte(password), 10)
 	return string(b), err
 }
+
+func checkPasswordHash(password, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil, err
+}
